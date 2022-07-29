2fcb30b8cb2b7f5f15848cdb221c68f2bf67117c
Майорский Виталий Станиславович <19733726@sigma.sbrf.ru>
2022-07-29 14:53:21 +0300

    my commit

---
+++ src/Homework/DZ_2/Poezd.java
@@ -0,0 +1,22 @@
+package Homework.DZ_2;
+
+public class Poezd {
+    public static void main(String[] args) {
+        Raspisanie Number_1 = new Raspisanie();
+        Raspisanie Number_2 = new Raspisanie();
+        Raspisanie Number_3 = new Raspisanie();
+        Number_1.platformNumber = 1;
+        Number_1.trainNumber = "1";
+        Number_1.otpravka();
+        Number_1.platformNumber();
+        Number_2.platformNumber = 2;
+        Number_2.trainNumber = "2";
+        Number_2.otpravka();
+        Number_2.platformNumber();
+        Number_3.platformNumber = 3;
+        Number_3.trainNumber = "3";
+        Number_3.otpravka();
+        Number_3.platformNumber();
+
+    }
+}
